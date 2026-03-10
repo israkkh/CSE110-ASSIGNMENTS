9b00feed09d452ec9d328f1e515914219df87086
israkkh <israkkh678@gmail.com>
2026-03-11 03:39:31 +0600

    task 7

--- Assignment 3/Task7.java
+++ Assignment 3/Task7.java
@@ -1,3 +1,18 @@
 public class Task7 {
+    public static void main(String[] args) {
+        int sum = 0 ; 
+        for(int i=1 ; i <=600 ; i++ ){
+            if((i % 7 == 0 || i % 9 == 0) && !(i % 7 == 0 && i % 9 == 0)){
+                sum += i ; 
+            }
+
+        } // initialize condition updet
+        System.out.println(sum);
+
+
+
+
+
+    }
     
 }
