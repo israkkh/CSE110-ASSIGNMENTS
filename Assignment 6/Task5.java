5095392c22bed735aaa2266385506c83cf91f7e7
israkkh <israkkh678@gmail.com>
2026-04-06 22:35:13 +0600

    task5 done w this shi

---
+++ Assignment 6/Task5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        int index = string.lastIndexOf(" ") ;
+        int count = string.length() - index - 1 ; 
+        System.out.println(count) ; 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
